a8b1ab654f0cb0625610b7b8e860e7562d7cacea
asepsetiawan9 <zep.setiawan09@gmail.com>
2022-08-21 09:35:46 +0700

    product, archive, category, db, etc,

--- src/models/product.js
+++ src/models/product.js
@@ -2,6 +2,8 @@ const db = require('../helpers/db');
 const {LIMIT_DATA} = process.env;
 
 exports.createProduct = (data, picture1, picture2, picture3, picture4, user_id, category_id, cb) => {
+  console.log('aaaaaaaa');
+  console.log(user_id);
   let value = [];
   const filter = {};
   const obj = {
@@ -40,7 +42,142 @@ exports.createProduct = (data, picture1, picture2, picture3, picture4, user_id, 
 };
 
 exports.showAllProduct=(searchBy,keyword,orderBy,order,limit=parseInt(LIMIT_DATA), offset=0,cb)=>{
-  const que = `SELECT * FROM products JOIN variant ON variant.product_id=products.id JOIN size ON size.variant_id=variant.id WHERE ${searchBy} LIKE '%${keyword}%' ORDER BY ${orderBy} ${order} LIMIT $1 OFFSET $2`;
+  const que = `SELECT 
+  product_name,
+  product_desc,
+  product_price,
+  product_stock,
+  product_condition,
+  picture1,
+  picture2,
+  picture3,
+  picture4, archive_status, category_name FROM product_details  INNER JOIN category  ON product_details.category_id  = category.id WHERE ${searchBy} LIKE '%${keyword}%' ORDER BY ${orderBy} ${order} LIMIT $1 OFFSET $2`;
+  const val = [limit,offset];
+  console.log(val);
+  db.query(que,val,(err,res)=>{
+    // console.log(err);
+    if(err){
+      cb(err);
+    }else{
+      cb(err,res);
+    }
+  });
+};
+
+exports.countAllProduct=(searchBy,keyword,cb)=>{
+  const que = `SELECT product_name,
+  product_desc,
+  product_price,
+  product_stock,
+  product_condition,
+  picture1,
+  picture2,
+  picture3,
+  picture4, archive_status, category_name FROM product_details  INNER JOIN category  ON product_details.category_id  = category.id WHERE ${searchBy} LIKE ${keyword}`;
+  db.query(que,(err,res)=>{
+    if(err){
+      cb(err);
+    }else{
+      cb(err,res.rowCount);
+    }
+  });
+};
+
+exports.editProduct = (id, user_id,data,picture1, picture2, picture3, picture4, cb) =>{
+  let value = [id];
+  const filtered = {};
+  const obj = {
+    archive_status: data.archive_status,
+    product_name: data.product_name,
+    product_desc: data.product_desc,
+    product_price: data.product_price,
+    product_stock: data.product_stock,
+    product_condition: data.product_condition,
+    user_id,
+    picture1,
+    picture2,
+    picture3,
+    picture4,
+  };
+  for(let i in obj){
+    if(obj[i]!==null){
+      if(obj[i]!==undefined){
+        filtered[i]=obj[i];
+        value.push(obj[i]);
+      }
+    }
+  }
+  const key = Object.keys(filtered);
+  const finallres = key.map((o,index)=>`${o}=$${index+2}`);
+  const que = `UPDATE product_details SET ${finallres} WHERE id=$1 RETURNING*`;
+  db.query(que,value,(err, res)=>{
+    if(res){
+      cb(err, res);
+    }else{
+      cb(err);
+    }
+  });
+};
+
+
+exports.archiveProduct = (id, user_id,data,picture1, picture2, picture3, picture4, cb) =>{
+  let value = [id];
+  const filtered = {};
+  const obj = {
+    archive_status: data.archive_status,
+    product_name: data.product_name,
+    product_desc: data.product_desc,
+    product_price: data.product_price,
+    product_stock: data.product_stock,
+    product_condition: data.product_condition,
+    user_id,
+    picture1,
+    picture2,
+    picture3,
+    picture4,
+  };
+  for(let i in obj){
+    if(obj[i]!==null){
+      if(obj[i]!==undefined){
+        filtered[i]=obj[i];
+        value.push(obj[i]);
+      }
+    }
+  }
+  const key = Object.keys(filtered);
+  const finallres = key.map((o,index)=>`${o}=$${index+2}`);
+  const que = `UPDATE product_details SET ${finallres} WHERE id=$1 RETURNING*`;
+  db.query(que,value,(err, res)=>{
+    if(res){
+      cb(err, res);
+    }else{
+      cb(err);
+    }
+  });
+};
+
+
+exports.deleteProduct = (id, cb) => {
+  const quer = 'DELETE FROM product_details WHERE id=$1 RETURNING *';
+  const value = [id];
+  db.query(quer, value, (err, res)=>{
+    if(err) {
+      cb(err);
+    }
+    cb(err, res.rows);
+  });
+};
+
+exports.showProductSeller=(id,searchBy,keyword,orderBy,order,limit=parseInt(LIMIT_DATA), offset=0,cb)=>{
+  const que = `SELECT product_name,
+  product_desc,
+  product_price,
+  product_stock,
+  product_condition,
+  picture1,
+  picture2,
+  picture3,
+  picture4, category_name FROM product_details  INNER JOIN category  ON product_details.category_id  = category.id WHERE ${searchBy} LIKE '%${keyword}%' AND user_id=${id} ORDER BY ${orderBy} ${order} LIMIT $1 OFFSET $2`;
   const val = [limit,offset];
   db.query(que,val,(err,res)=>{
     if(err){
@@ -51,8 +188,16 @@ exports.showAllProduct=(searchBy,keyword,orderBy,order,limit=parseInt(LIMIT_DATA
   });
 };
 
-exports.countAllProducts=(searchBy,keyword,cb)=>{
-  const que = `SELECT * FROM products JOIN variant ON variant.product_id=products.id JOIN size ON size.variant_id=variant.id WHERE ${searchBy} LIKE ${keyword}`;
+exports.countshowProductSeller=(id,searchBy,keyword,cb)=>{
+  const que = `SELECT product_name,
+  product_desc,
+  product_price,
+  product_stock,
+  product_condition,
+  picture1,
+  picture2,
+  picture3,
+  picture4, category_name FROM product_details  INNER JOIN category  ON product_details.category_id  = category.id WHERE ${searchBy} LIKE ${keyword} AND user_id=${id}`;
   db.query(que,(err,res)=>{
     if(err){
       cb(err);
@@ -62,12 +207,82 @@ exports.countAllProducts=(searchBy,keyword,cb)=>{
   });
 };
 
-// SELECT product_name
-// product_desc,
-// product_price,
-// product_stock,
-// product_condition,
-// picture1,
-// picture2,
-// picture3,
-// picture4, category_name FROM product_details  INNER JOIN category  ON product_details.category_id  = category.id;
+exports.showProductByCategory=(category_name,searchBy,keyword,orderBy,order,limit=parseInt(LIMIT_DATA), offset=0,cb)=>{
+
+  const que = `SELECT product_name,
+  product_desc,
+  product_price,
+  product_stock,
+  product_condition,
+  picture1,
+  picture2,
+  picture3,
+  picture4, category_name FROM product_details  INNER JOIN category   ON product_details.category_id  = category.id WHERE ${searchBy} LIKE '%${keyword}%' AND category_name='${category_name}' ORDER BY ${orderBy} ${order} LIMIT $1 OFFSET $2`;
+  const val = [limit,offset];
+  db.query(que,val,(err,res)=>{
+    if(err){
+      cb(err);
+    }else{
+      cb(err,res);
+    }
+  });
+};
+
+exports.countProductByCategory=(category_name,searchBy,keyword,cb)=>{
+  const que = `SELECT product_name,
+  product_desc,
+  product_price,
+  product_stock,
+  product_condition,
+  picture1,
+  picture2,
+  picture3,
+  picture4, category_name FROM product_details  INNER JOIN category  ON product_details.category_id  = category.id WHERE ${searchBy} LIKE ${keyword} AND category_name='${category_name}'`;
+  db.query(que,(err,res)=>{
+    if(err){
+      cb(err);
+    }else{
+      cb(err,res.rowCount);
+    }
+  });
+};
+
+exports.showProductByArchive=(archive_status,searchBy,keyword,orderBy,order,limit=parseInt(LIMIT_DATA), offset=0,cb)=>{
+
+  const que = `SELECT product_name,
+  product_desc,
+  product_price,
+  product_stock,
+  product_condition,
+  picture1,
+  picture2,
+  picture3,
+  picture4, archive_status, category_name FROM product_details  INNER JOIN category   ON product_details.category_id  = category.id WHERE ${searchBy} LIKE '%${keyword}%' AND archive_status=${archive_status} ORDER BY ${orderBy} ${order} LIMIT $1 OFFSET $2`;
+  const val = [limit,offset];
+  db.query(que,val,(err,res)=>{
+    if(err){
+      cb(err);
+    }else{
+      cb(err,res);
+    }
+  });
+};
+
+exports.countProductByArchive=(archive_status,searchBy,keyword,cb)=>{
+  const que = `SELECT product_name,
+  product_desc,
+  product_price,
+  product_stock,
+  product_condition,
+  picture1,
+  picture2,
+  picture3,
+  picture4, category_name FROM product_details  INNER JOIN category  ON product_details.category_id  = category.id WHERE ${searchBy} LIKE ${keyword} AND archive_status=${archive_status}`;
+  db.query(que,(err,res)=>{
+    if(err){
+      cb(err);
+    }else{
+      cb(err,res.rowCount);
+    }
+  });
+};
--- src/routers/product.js
+++ src/routers/product.js
@@ -1,35 +1,17 @@
 const product = require('express').Router();
 const productController = require('../controllers/product');
-const { body } = require('express-validator');
-const validationCheck = require('../middleware/checkValidation');
+// const { body } = require('express-validator');
+// const validationCheck = require('../middleware/checkValidation');
 const upload = require('../middleware/upload');
 const authMiddle = require('../middleware/auth');
 
-// const registerSellerValidator = [
-//   body('email')
-//     .notEmpty().withMessage('Email Required')
-//     .isEmail().withMessage('Wrong Email Format'),
-//   body('password')
-//     .notEmpty().withMessage('Password Required')
-//     .isLength({min: 4}).withMessage('Password must be more than 4 characters')
-//     .customSanitizer(
-//       async val =>{
-//         const hash = await bcrypt.hash(val, 10);
-//         return hash;
-//       })
-// ];
-
-// const loginValidator =  [
-//   body('email')
-//     .exists({checkFalsy: true}).withMessage('Enter an Email')
-//     .isEmail().withMessage('Wrong Email Format'),
-
-//   body('password')
-//     .exists({checkFalsy: true}).withMessage('Enter a Password')
-// ];
-
 product.post('/', authMiddle, upload, productController.createProduct);
-// product.patch('/', ...loginValidator, validationCheck, authController.login);
+product.get('/', productController.showAllProduct);
+product.get('/ProductSeller',authMiddle,productController.showProductSeller);
+product.get('/:category_name',authMiddle,productController.showProductByCategory);
+product.get('/archive/:archive_status',authMiddle,productController.showProductByArchive);
+product.patch('/:id',authMiddle,upload,productController.editProduct);
+product.delete('/:id',authMiddle, productController.deleteProduct);
 
 module.exports = product ;
 
