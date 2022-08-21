3ddd4b22a7598d8a264579557ea1a2182a437fde
asepsetiawan9 <zep.setiawan09@gmail.com>
2022-08-21 13:38:55 +0700

    chat, Postman, DB, fix profile and auth + product

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,11 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-router.use("/auth", require("./auth"));
-router.use("/product", require("./product"));
-router.use("/profile", require("./profile"));
-router.use("/add_chart", require("./transaction"));
-router.use("/get_chart", require("./transaction"));
-router.use("/payment", require("./payment"));
+router.use('/auth', require('./auth'));
+router.use('/chat', require('./chat'));
+router.use('/product', require('./product'));
+router.use('/profile', require('./profile'));
+router.use('/add_chart', require('./transaction'));
+router.use('/get_chart', require('./transaction'));
+router.use('/payment', require('./payment'));
 
 module.exports = router;
