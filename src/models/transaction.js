fd89e7f5aec5080348a9a5cbf1cf0f679e2c671e
refaldy04 <refaldybanggana@gmail.com>
2022-08-22 12:14:46 +0700

    fix code

--- src/models/transaction.js
+++ src/models/transaction.js
@@ -17,7 +17,6 @@ exports.createCart = (data, cb) => {
 
 exports.getCartById = (id, cb) => {
   db.query("SELECT * FROM cart WHERE user_id=$1", [id], (err, res) => {
-    // console.log(res);
     cb(err, res.rows);
   });
 };
